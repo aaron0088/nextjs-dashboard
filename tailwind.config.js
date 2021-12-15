485291ea519821bd4506a7216aade1f67b9297ca
aa.aaron0088 <aa.aaron0088@gmail.com>
2021-12-15 17:11:17 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "jit",
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    
+  ],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'background-pattern': "url('/public/Form-bg.svg')"
+      }
+    },
+  },
+  plugins: [],
+}
